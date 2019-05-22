298898f4c42ff51f71c0f907c5a6aeebb78b8f1d
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-05-21 22:06:16 -0400

    example fully working but uses csv

--- Chris/csv_method.py
+++ Chris/csv_method.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def csv_to_pd(path_to_csv):
     df_2 = pd.read_csv(path_to_csv)
     df_2.drop(['Unnamed: 0'], axis=1, inplace=True)
--- example.py
+++ example.py
@@ -1,11 +1,14 @@
 import sys
 
 sys.path.append('./Middleware')
+sys.path.append('./Chris')
+
+from csv_method import csv_to_pd
 
 from middleware import Settings,panda_to_mongo
 
 settings = Settings("prac_db","prac_collection",'mongodb://localhost:27017/')
 
-df = None
+df = csv_to_pd("./Chris/project_2.csv")
 
 panda_to_mongo(df,settings)
